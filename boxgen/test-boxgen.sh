e28e5702817110b9f628911823d1521fe06e92ad
Matteo Concas <mconcas@cern.ch>
2019-07-03 14:59:06 +0200

    Add pythia8 test

---
+++ boxgen/test-boxgen.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash -e
+
+export WORK_DIR=${WORK}/alice/alice/sw 
+source ${WORK_DIR}/slc7_x86-64/O2/latest/etc/profile.d/init.sh
+export PATH=$PATH:${WORK_DIR}/slc7_x86-64/O2/latest/bin
+export VMCWORKDIR=${O2_ROOT}/share
+
+which o2-sim
+o2-sim-serial -n 1 -m TPC 2>&1 | tee -a o2sim.log
---
+++ pythia8/test-pythia8.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash -e
+
+export WORK_DIR=${WORK}/alice/alice/sw 
+source ${WORK_DIR}/slc7_x86-64/O2/latest/etc/profile.d/init.sh
+export PATH=$PATH:${WORK_DIR}/slc7_x86-64/O2/latest/bin
+export VMCWORKDIR=${O2_ROOT}/share
+
+o2-sim -j 4 -n 20 -m PIPE TPC ITS -g pythia8
