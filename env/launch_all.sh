798e3b4d8d8263305f5c4e4498bfe9875e3c98d0
Dario Berzano <dario.berzano@gmail.com>
2019-08-27 18:16:07 +0200

    Add new massive launch config

--- env/launch_all.sh
+++ env/launch_all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 DRY='echo DRYRUN +'
 [[ $1 == --for-real ]] && DRY=
-#for NEVT in 400 600 800; do
-#  for NJOB in 2 5 10 15 18 20 25 40 60 75 90 100 150 200; do
-for NEVT in 10; do
-  for NPROC in 20; do
-    $DRY sbatch -J simpythia8_testrun2 submit.sh --events $NEVT --processes $NPROC --jobs 10 --step sim
+
+for NINST in 1 2 4 6 8 10 11 12 13; do
+  for NEVT in 1200 1600; do
+    for NPROC in 20; do
+      $DRY sbatch -J simpythia8_multijobs2 submit.sh --events $NEVT --processes $NPROC --jobs $NINST --step sim
+      #                                              ^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^
+      #                                              total num evt  parallel fmq proc  n o2-sim inst
+    done
   done
 done
